c07334f5db6439a7618054e5edf10f578d9bc615
haberger <74511220+haberger@users.noreply.github.com>
2023-02-04 18:45:51 +0100

    Update osmesa-install.sh
    print echo

--- osmesa-install/osmesa-install.sh
+++ osmesa-install/osmesa-install.sh
@@ -25,7 +25,7 @@ llvmprefix="${LLVM_PREFIX:-/opt/llvm}"
 
 set -e # Exit immediately if a command exits with a non-zero status
 set -u # Treat unset variables as an error when substituting.
-#set -x # Print commands and their arguments as they are executed.
+set -x # Print commands and their arguments as they are executed.
 
 # mesa version
 mesaversion="${OSMESA_VERSION:-17.1.6}"
